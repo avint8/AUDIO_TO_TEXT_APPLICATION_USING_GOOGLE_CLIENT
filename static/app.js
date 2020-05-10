9e05ee73e841f76450fb6f687453e05d26cffd1d
avint <avint8@gmail.com>
2020-05-10 17:04:48 +0530

    added bootstrap

--- static/app.js
+++ static/app.js
@@ -87,7 +87,8 @@ function stopRecording() {
 }
 
 function createDownloadLink(blob) {
-
+    var rec = document.getElementById('rec');
+    var conv=document.getElementById('conv');
     var url = URL.createObjectURL(blob);
     var au = document.createElement('audio');
     var li = document.createElement('li');
@@ -95,13 +96,15 @@ function createDownloadLink(blob) {
 
     //name of .wav file to use during upload and download (without extendion)
     var filename = new Date().toISOString();
-
+    rec.style.display="block";
+    conv.style.display="block";
     //add controls to the <audio> element
     au.controls = true;
     au.src = url;
 
     //save to disk link
     link.href = url;
+    link.setAttribute("class","btn btn-primary btn-lg");
     link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
     link.innerHTML = "Save to disk";
 
@@ -118,6 +121,7 @@ function createDownloadLink(blob) {
     var upload = document.createElement('button');
     //upload.href="#";
     upload.innerHTML = "Upload";
+    upload.setAttribute("class","btn btn-primary btn-lg")
     upload.addEventListener("click", function(event){
           var xhr=new XMLHttpRequest();
           xhr.onload=function(e) {
